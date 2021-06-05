1f5b26b3b56d876bc922bdbc76227ffa45c08e09
ayumi-tulips <snowcat.kuroneko@gmail.com>
2021-06-05 21:10:00 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/ayumi/intent/MainActivity.kt
@@ -0,0 +1,23 @@
+package jp.techacademy.ayumi.intent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+
+class MainActivity : AppCompatActivity(),View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
+    }
+}

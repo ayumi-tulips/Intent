ec4cd9c07265137ec76e8030c124b23ecbaa472b
ayumi-tulips <snowcat.kuroneko@gmail.com>
2021-06-09 21:17:31 +0900

    a

--- app/src/main/java/jp/techacademy/ayumi/intent/SecondActivity2.kt
+++ app/src/main/java/jp/techacademy/ayumi/intent/SecondActivity2.kt
@@ -3,14 +3,17 @@ package jp.techacademy.ayumi.intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.textView
+import kotlinx.android.synthetic.main.activity_second2.*
+
 
 class SecondActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second2)
-val value1 = intent.getIntExtra("VALUE1",0)
-val value2 = intent.getIntExtra("VALUE2",0)
+        val value1 = intent.getIntExtra("VALUE1",0)
+        val value2 = intent.getIntExtra("VALUE2",0)
 
-textView.text = "${value1 + value2}"
+textView1.text = "${value1 + value2}"
   }
 }
